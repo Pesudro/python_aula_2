d9261721caa205032e40f4745c7166b5397990c9
Pesudro <pedrohenriquemedeirostomasi@gmail.com>
2019-08-26 19:00:13 -0300

    exercicio 14

---
+++ exercicio_14.py
@@ -0,0 +1,7 @@
+"""14 - Escreva um algoritmo que calcule e imprima a tabuada do 8 (1 a 10)."""
+tabuada = [1,2,3,4,5,6,7,8,9,10]
+
+numero = int("Digite o numero que você deseja ver a tabuada:")
+
+for i in tabuada:
+    print(i*numero)
